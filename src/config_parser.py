f467308977a3f3b3567ab780ddc03cb5961f1330
lc3267 <melvin.strobl@kit.edu>
2024-04-05 15:48:24 +0200

    config parser fixes
    Signed-off-by: lc3267 <melvin.strobl@kit.edu>

--- src/config_parser.py
+++ src/config_parser.py
@@ -11,40 +11,55 @@ class ConfigParser:
     def __init__(self):
         parser = ArgumentParser()
 
-        parser.add_argument("--config_dir", type=str, default="config.yaml")
-        parser.add_argument("--data_dir", type=str, default="./data")
-        parser.add_argument("--seed", type=int, default=100)
-        parser.add_argument("--image_size", type=int, default=12)
-        parser.add_argument("--batch_size", type=int, default=20)
-        parser.add_argument("--epochs", type=int, default=100)
-        parser.add_argument("--n_figures", type=int, default=10)
-        parser.add_argument("--n_samples", type=int, default=400)
-        parser.add_argument("--n_qubits", type=int, default=8)
-        parser.add_argument("--latent_size", type=int, default=100)
-        parser.add_argument("--n_layers", type=int, default=22)
-        parser.add_argument("--d_lr", type=float, default=0.0002)
-        parser.add_argument("--c_lr", type=float, default=0.0002)
-        parser.add_argument("--q_lr", type=float, default=0.01)
-        parser.add_argument("--generator_update_step", type=int, default=3)
-        parser.add_argument("--noise_gain", type=float, default=0.5)
-        parser.add_argument("--noise_offset", type=float, default=0.0)
-        parser.add_argument("--selected_label", type=int, default=0)
-        parser.add_argument("--tracking_uri", type=str, default="http://127.0.0.1:5000")
-        parser.add_argument("--log_level", type=str, default="INFO")
+        parser.add_argument("--config_file", type=str, default="config.yaml")
+        parser.add_argument("--seed", type=int)
+        parser.add_argument("--image_size", type=int)
+        parser.add_argument("--batch_size", type=int)
+        parser.add_argument("--epochs", type=int)
+        parser.add_argument("--n_figures", type=int)
+        parser.add_argument("--n_samples", type=int)
+        parser.add_argument("--n_qubits", type=int)
+        parser.add_argument("--latent_size", type=int)
+        parser.add_argument("--n_layers", type=int)
+        parser.add_argument("--disc_lr", type=float)
+        parser.add_argument("--gen_lr", type=float)
+        parser.add_argument("--generator_update_step", type=int)
+        parser.add_argument("--noise_gain", type=float)
+        parser.add_argument("--noise_offset", type=float)
+        parser.add_argument("--selected_label", type=int)
+        parser.add_argument("--tracking_uri", type=str)
+        parser.add_argument("--log_level", type=str)
+        parser.add_argument("--model", type=str)
 
         self.args = parser.parse_args()
 
     def get_config(self):
         if os.path.exists(self.args.config_dir):
+            log.info(f"Reading config from {self.args.config_dir}")
             with open(self.args.config_dir, "r") as stream:
                 try:
                     config = yaml.safe_load(stream)
                 except yaml.YAMLError as exc:
-                    print(exc)
+                    log.error(f"Error while loading config file")
+                    raise yaml.YAMLError(exc)
 
         else:
             log.warning(
                 f"No config file {self.args.config_dir} found. Using default values."
             )
             config = {}
-        return config or self.args
+
+        # Iterate all arguments
+        for k, v in self.args.__dict__.items():
+            # check if the argument is provided
+            if v is None:
+                # check if the config file has the argument
+                if k not in config.keys():
+                    raise KeyError(f"Key {k} not found in config file.")
+                # replace the value with the one from the config file
+                v = config[k]
+            else:
+                log.debug(f"Using provided argument {k}: {v}")
+            # set the value to the namespace
+            setattr(self.args, k, v)
+        return self.args
